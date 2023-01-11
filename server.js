5485bce5c33d48e7846d65bd1b5c3bac0562c871
ChrisDOE <chris.sean.86@gmail.com>
2023-01-11 08:55:41 +0100

    nodejs modules imported
    - modules imported: 'http' 'url' 'fs'
    
    - server.js creates a server (port:8080) and listenes.
    - parsing for 'documnetation' and returns .this html file. if not, back to index.
    - creates a log in log.txt for each request incl timestamp

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
+const http = require('http'),
+url = require('url'),
+fs = require('fs');
 
-const http = require('http');
 http.createServer((request, response) => {
-response.writeHead(200, {'Content-Type': 'text/plain'
-});
-response.end('Hello Node\n');
+
+    let addr = request.url,
+    q = url.parse(addr, true),
+    filepath = '';
+
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+        if (err) {
+            throw err;
+            console.log('error!');
+        }
+        console.log('Added to log.txt!');
+    });
+
+    if (q.pathname.includes('documentation')) {
+        filepath = (__dirname + '/documentation.html');
+    } else {
+        filepath = 'index.html'
+    }
+
+    fs.readFile(filepath, (err, data) => {
+        if (err) {
+            throw err;
+            console.log('error!');
+        }
+
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    });
+
 }).listen(8080);
+
 console.log('My first Node test server is running on Port 8080.');
--- test.js
+++
@@ -1,5 +0,0 @@
-
-console.log("Hello Node!");
-
-console.log("Hello Node!");
-console.log("Goodbye.");
