43ea458d5a2fa2933d7851bc86705ccc9b96d842
ChrisDOE <chris.sean.86@gmail.com>
2023-01-18 10:43:11 +0100

    express, morgan module
    - 2 new modules installed
    - new GET route located at /movies to show a json file with random top 10 movies
    - created an error handling middleware function

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# MyFlix-App
+## MyFlix-App
 
 A tutorial by [CareerFoundry](https://careerfoundry.com) to work with Node.js
 
 To build the server-side component of a “movies” web application. The web application will provide users with access to information about different movies, directors, and genres. Users will be able to sign up, update their personal information, and create a list of their favorite movies.
 
-# Goal of this Achievment:
+## Goal of this Achievment:
 
 > In the next Achievement, you’ll build the client-side component of this same application using REACT. By the end of Achievement 3, you’ll have a complete web application (client-side and server-side) built using full-stack JavaScript technologies that you can showcase in your portfolio. The project will demonstrate your mastery of full-stack JavaScript development, including APIs, web server frameworks, databases, business logic, authentication, data security, and more. The complete tech stack you’ll master is known as the MERN (MongoDB, Express, React, and Node.js) stack.
- 
-# Included features (in progress)
+
+## Included features (in progress)
 
 - Return a list of ALL movies to the user
 - Return data (description, genre, director, image URL, whether it’s featured or not) about a
--- index.js
+++ index.js
@@ -1,19 +1,88 @@
-const express = require('express');
-const http = require('http'),
-url = require('url');
+const express = require('express'),
+morgan = require('morgan'),
+fs = require('fs'),
+path = require ('path');
+
 const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
+
+app.use(morgan('combined', {stream: accessLogStream}));
 
 
-http.createServer((req, res) => {
-    let reqURL = url.parse(req.url, true);
-    if (reqURL.pathname == '/documentation.html') {
-        res.writeHead(200, {'Content-Type': 'test/plain'
+let topMovies = [
+    {
+        title: 'The Matrix',
+        genre: 'SciFi',
+        year: '1999'
+    },
+    {
+        title: 'Inception',
+        genre: 'Thriller',
+        year: '2010'
+    },
+    {
+        title: 'The Lord of the Rings: The Return of the King',
+        genre: 'Fantasy',
+        year: '2003'
+    },
+    {
+        title: 'The Terminator',
+        genre: 'SciFi',
+        year: '1984'
+    },
+    {
+        title: 'The Silence of the Lambs',
+        genre: 'Thriller',
+        year: '1991'
+    },
+    {
+        title: 'Harry Potter and the Sorcerer\'s Stone',
+        genre: 'Fantasy',
+        year: '2001'
+    },
+    {
+        title: 'Blade Runner',
+        genre: 'SciFi',
+        year: '1982'
+    },
+    {
+        title: 'Jurassic Park',
+        genre: 'SciFi',
+        year: '1993'
+    },
+    {
+        title: 'The Exorcist',
+        genre: 'Thriller',
+        year: '1973'
+    },
+    {
+        title: 'Interstellar',
+        genre: 'SciFi',
+        year: '2014'
+    },
+
+
+];
+
+app.get('/', (req, res) => {
+    res.send('\"The only true wisdom is in knowing you know nothing.\"- Socrates')
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+app.get('/documentation.html', (req, res) => {
+    res.sendFile('public/documentation.html', {
+        root: __dirname
     });
-res.end('Documentation on the bookclub API.\n');
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Welcome to my book club!\n');
-    }
-}).listen(8080);
+});
 
-console.log('My first Node test server is running on Port 8080.');
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+    console.log('Aour app is listening to port 8080.');
+});
